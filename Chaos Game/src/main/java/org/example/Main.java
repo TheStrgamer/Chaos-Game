70decadb0489f621ac181608632beb4a58ba68ea
Konrad <konrados@stud.ntnu.no>
2024-02-08 12:19:43 +0100

    Changed main message

--- Chaos Game/src/main/java/org/example/Main.java
+++ Chaos Game/src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Nisse Dingoer");
+    System.out.println("Ber er hver að baki nema sér bróður eigi");
   }
 }
