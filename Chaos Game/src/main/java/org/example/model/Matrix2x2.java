ba68259d24521f3c02ed5305d8f8ba40cec784bb
Konrad <konrados@stud.ntnu.no>
2024-02-08 12:46:13 +0100

    Added variables to matrix class

--- Chaos Game/src/main/java/org/example/model/Matrix2x2.java
+++ Chaos Game/src/main/java/org/example/model/Matrix2x2.java
@@ -1,6 +1,8 @@
 package org.example.model;
 
 public class Matrix2x2 {
-  private double x1;
-  private double x2;
+  private double a00;
+  private double a01;
+  private double a10;
+  private double a11;
 }
