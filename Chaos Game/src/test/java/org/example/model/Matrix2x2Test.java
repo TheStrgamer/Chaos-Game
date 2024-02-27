b9d0ec52ac6fc6e36dec9767e3422a30319ff3a4
Mathias Hølestøl <mathiash@uia.no>
2024-02-27 17:34:47 +0100

    Added test class Matrix2x2

---
+++ Chaos Game/src/test/java/org/example/model/Matrix2x2Test.java
@@ -0,0 +1,60 @@
+package org.example.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class Matrix2x2Test {
+
+  @Nested
+  @DisplayName("Positive tests")
+  class PositiveTests {
+
+    @Test
+    @DisplayName("Multiply method returns correct vector")
+    void multiplyMethodCalculatesCorrectlyFirstValues() {
+      try {
+        Matrix2x2 matrix = new Matrix2x2(1, 2, 3, 4);
+        Vector2D result_vector = matrix.multiply(new Vector2D(1, 2));
+        assertEquals(5, result_vector.getX0());
+        assertEquals(11, result_vector.getX1());
+      } catch (Exception e) {
+        fail("An exception was thrown");
+      }
+    }
+
+    @Test
+    @DisplayName("Multiply method returns correct vector")
+    void multiplyMethodCalculatesCorrectlySecondValues() {
+      try {
+        Matrix2x2 matrix = new Matrix2x2(3, 3, 4, 4);
+        Vector2D result_vector = matrix.multiply(new Vector2D(5, 0));
+        assertEquals(15, result_vector.getX0());
+        assertEquals(20, result_vector.getX1());
+      } catch (Exception e) {
+        fail("An exception was thrown");
+      }
+    }
+  }
+
+  @Nested
+  @DisplayName("Negative Tests")
+  class NegativeTests {
+
+    @Test
+    @DisplayName("Multiply method throws exception when given vector is null")
+    void testSubtractMethodThrowsOnNull() {
+      try {
+        Matrix2x2 matrix = new Matrix2x2(1, 2, 3, 4);
+        Vector2D vector = null;
+        matrix.multiply(vector);
+        fail("An exception was not thrown");
+      } catch (Exception e) {
+        assertEquals(e.getMessage(),"Vector cannot be null");
+      }
+    }
+  }
+}
